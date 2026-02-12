f4ffad19790d5e8f1682b3fbd076b2b5aee7a581
achrefmabrouk <achrefmabrouk049@gmail.com>
2026-02-12 13:07:46 +0100

    first task

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './core/header/header.component';
+import { FooterComponent } from './core/footer/footer.component';
+import { ListSuggestionComponent } from './core/list-suggestion/list-suggestion.component';
+import { HomeComponent } from './home/home.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+import { NavComponent } from './nav/nav.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    ListSuggestionComponent,
+    HomeComponent,
+    NotFoundComponent,
+    NavComponent
   ],
   imports: [
     BrowserModule,
